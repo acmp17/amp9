9ad6f4a7a361a7321dbaf75cc40cb0bf02e04096
acmp17 <alliewxy@gmail.com>
2022-03-06 22:06:17 -0500

    simple_pages_test.py change 2 - because the comments were wrong lol. page1.html change 1 - adding git description git_page_test.py change 1 - adding it to repo and testing git description

---
+++ tests/git_page_test.py
@@ -0,0 +1,8 @@
+"""This test is for the git page"""
+
+def test_request_git(client):
+    """This confirms content on the git page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"<h1>Git</h1>" in response.data
+    assert b"<p>Git is the most popular version control system in the world which makes it easier to track changes in a file. It is a great tool to coordinate with other developers and save “checkpoints” on what is changed over time. You can download Git by it’s command-line interface Github is the website that is used for hosting the projects that use git.</p>" in response.data
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -17,36 +17,36 @@ def test_request_index(client):
     assert b"Index Page" in response.data
 
 def test_request_about(client):
-    """This makes the index page"""
+    """This makes the about page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
 
 def test_request_page1(client):
-    """This makes the index page"""
+    """This makes the page1"""
     response = client.get("/page1")
     assert response.status_code == 200
     assert b"Page 1" in response.data
 
 def test_request_page2(client):
-    """This makes the index page"""
+    """This makes the page2"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Page 2" in response.data
 
 def test_request_page3(client):
-    """This makes the index page"""
+    """This makes the page3"""
     response = client.get("/page3")
     assert response.status_code == 200
     assert b"Page 3" in response.data
 
 def test_request_page4(client):
-    """This makes the index page"""
+    """This makes the page4"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"Page 4" in response.data
 
 def test_request_page_not_found(client):
-    """This makes the index page"""
+    """This makes the page not found page"""
     response = client.get("/page5")
     assert response.status_code == 404
