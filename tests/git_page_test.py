6900dccd97f9b1238b1182a17c129e87509ad6ed
acmp17 <alliewxy@gmail.com>
2022-03-06 22:25:55 -0500

    git_page_test.py change 3 - testing again because i forgot to add the last tag of </p>

--- tests/git_page_test.py
+++ tests/git_page_test.py
@@ -5,6 +5,6 @@ def test_request_git(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"<h1>Git</h1>" in response.data
-    assert b"<p>Git is the most popular version control system in the world which makes it easier to track changes in a file. " in response.data
+    assert b"<p>Git is the most popular version control system in the world which makes it easier to track changes in a file." in response.data
     assert b"It is a great tool to coordinate with other developers and save checkpoints on what is changed over time." in response.data
-    assert b"You can download Git by it's command-line interface. Github is the website that is used for hosting the projects that use git." in response.data
+    assert b"You can download Git by it's command-line interface. Github is the website that is used for hosting the projects that use git.</p>" in response.data
