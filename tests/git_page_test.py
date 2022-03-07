082f500d8059f620082083fdf82598de4b211c9c
acmp17 <alliewxy@gmail.com>
2022-03-06 22:33:23 -0500

    git_page_test.py change 4 - testing again because changed client.get to page 1 ???

--- tests/git_page_test.py
+++ tests/git_page_test.py
@@ -2,7 +2,7 @@
 
 def test_request_git(client):
     """This confirms content on the git page"""
-    response = client.get("/")
+    response = client.get("/page1")
     assert response.status_code == 200
     assert b"<h1>Git</h1>" in response.data
     assert b"<p>Git is the most popular version control system in the world which makes it easier to track changes in a file." in response.data
