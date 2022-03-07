2b7accb21f37fa4b92b3bef1d8d362e1ff5fc391
acmp17 <alliewxy@gmail.com>
2022-03-07 12:47:06 -0500

    docker_page_test.py - added test for docker commands list page2.html - added docker commands list

--- tests/docker_page_test.py
+++ tests/docker_page_test.py
@@ -1,10 +1,16 @@
 """This test is for the docker page"""
 
 def test_request_docker(client):
-    """This confirms content on the git page"""
+    """This confirms content on the docker page"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"<h1>Docker</h1>" in response.data
     assert b"<h1>Containers</h1>" in response.data
     assert b"<h1>Images</h1>" in response.data
     assert b'<a href="https://hub.docker.com/repository/docker/acmp17/project1-amp">Visit DockerHub Project Repository</a>' in response.data
+
+def test_docker_commands_list(client):
+    """This confirms content in the docker commands list"""
+    response =  client.get("/page2")
+    assert response.status_code == 200
+    assert b"Docker Commands to Know" in response.data
