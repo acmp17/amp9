8f2a8e5fa701d1b13db1f5d14674420b2a356f24
acmp17 <alliewxy@gmail.com>
2022-03-07 23:59:33 -0500

    changed the testing code

--- tests/cont_int_deploy_page_test.py
+++ tests/cont_int_deploy_page_test.py
@@ -1,5 +1,5 @@
 def test_content_on_cont_int_deploy(client):
-    """This confirms content on the git page"""
+    """This confirms content on the cont int page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"<h2>Continuous Integration</h2>" in response
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,6 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
     assert b'<a class="nav-link" href="/page1">Git</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
