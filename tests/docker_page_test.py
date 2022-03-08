587163fc068f4fdef7d04b4740ee8fd598766e45
acmp17 <alliewxy@gmail.com>
2022-03-07 21:46:35 -0500

    making a change

--- tests/docker_page_test.py
+++ tests/docker_page_test.py
@@ -4,19 +4,19 @@ def test_request_docker(client):
     """This confirms content on the docker page"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"<h1>Docker</h1>" in response.data
-    assert b"<h1>Containers</h1>" in response.data
-    assert b"<h1>Images</h1>" in response.data
+    assert b"<h2>Docker</h2>" in response.data
+    assert b"<h2>Containers</h2>" in response.data
+    assert b"<h2>Images</h2>" in response.data
     assert b'<a href="https://hub.docker.com/repository/docker/acmp17/project1-amp">Visit DockerHub Project Repository</a>' in response.data
 
 def test_docker_commands_list(client):
     """This confirms content in the docker commands list"""
     response =  client.get("/page2")
     assert response.status_code == 200
-    assert b"<h1>Docker Commands to know</h1>" in response.data
+    assert b"<h2>Docker Commands to know</h2>" in response.data
 
 def test_docker_starter(client):
     """This tests the docker starter description"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"<h1>How to start a Project with Docker</h1>" in response.data
+    assert b"<h2>How to start a Project with Docker</h2>" in response.data
