677ee1b76a3227a1df290f0c490635d01062876d
acmp17 <alliewxy@gmail.com>
2022-03-07 19:08:09 -0500

    changes in h1 tags

--- tests/git_page_test.py
+++ tests/git_page_test.py
@@ -4,7 +4,7 @@ def test_request_git(client):
     """This confirms content on the git page"""
     response = client.get("/page1")
     assert response.status_code == 200
-    assert b"<header>All About Git</header>" in response.data
+    assert b"<h1>All About Git</h1>" in response.data
     assert b"<h2>Git</h2>" in response.data
     assert b"<h2>Branches</h2>" in response.data
     assert b"<h2>Merge</h2>" in response.data
