3f598a31d2c0d31d7a0a3541c7355d70a0563dae
acmp17 <alliewxy@gmail.com>
2022-03-28 15:30:28 -0400

    took out index.html test content page1 correct format

--- tests/index_page_test.py
+++ tests/index_page_test.py
@@ -1,10 +1,1 @@
-def test_index_page_content(client):
-    """This tests the content on the index.html page"""
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b'{% extends "base.html" %}' \
-           b'{% block content %}' \
-           b'<!-- Carousel -->' \
-           b'<!doctype html>' \
-           b'<html lang="en">' \
-           b'<head>' in response.data
+
