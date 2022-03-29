7e2864e5943266e5de14d6f845b7de7673196867
acmp17 <alliewxy@gmail.com>
2022-03-28 22:22:48 -0400

    testing tests on index_page_test.py

--- tests/index_page_test.py
+++ tests/index_page_test.py
@@ -1,1 +1,11 @@
-
+def test_index_page_content(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'{% extends "base.html" %}' in response.data
+    assert b'{% block content %}' in response.data
+    assert b'<!-- Carousel -->' in response.data
+    assert b'<!doctype html>' in response.data
+    assert b'<html lang="en">' in response.data
+    assert b'<head>' in response.data
+    assert b'<!-- Required meta tags -->' in response.data
