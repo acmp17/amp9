1cef675410a54cdb6ce5c326106cd569dbb8d153
acmp17 <alliewxy@gmail.com>
2022-03-28 23:46:57 -0400

    added another reference

--- tests/git_page_test.py
+++ tests/git_page_test.py
@@ -1,6 +1,7 @@
 """This test is for the git page"""
 
-def test_git_page_contant(client):
+
+def test_git_page_content(client):
     """This confirms content on the git page"""
     response = client.get("/page1")
     assert response.status_code == 200
@@ -16,3 +17,9 @@ def test_git_page_contant(client):
     assert b'<h2>Git</h2>' in response.data
     assert b"""<p>Git is the most popular version control system in the world which makes it easier to track changes in a file.
             It is a great tool to coordinate with other developers and save checkpoints on what is changed over time.""" in response.data
+    assert b"""You can download Git by its commandline interface. Github is the website that is used for hosting the
+            projects that use git.</p>""" in response.data
+    assert b'<h2>Branches</h2>' in response.data
+    assert b"""<p>Branches that are being used in git are similar to how File > Save as.. In a text editor.
+            The branch name is for reference of the current state of your project.
+            One branch can be for working on a single feature and another branch can be for applying another feature</p>""" in response.data
