82e2579e80e4864c62f3f37df0d25c0e9865b743
acmp17 <alliewxy@gmail.com>
2022-03-28 22:31:13 -0400

    without / index_page_test.py

--- tests/index_page_test.py
+++ tests/index_page_test.py
@@ -1,9 +1,13 @@
 def test_index_page_content(client):
     """This makes the index page"""
-    response = client.get("/index")
+    response = client.get("/")
     assert response.status_code == 200
     assert b'<!-- Carousel -->' in response.data
     assert b'<!doctype html>' in response.data
     assert b'<html lang="en">' in response.data
     assert b'<head>' in response.data
     assert b'<!-- Required meta tags -->' in response.data
+    assert b'<meta charset="utf-8">' in response.data
+    assert b'<meta name="viewport" content="width=device-width, initial-scale=1">' in response.data
+    assert b'<!-- Bootstrap CSS -->' in response.data
+    assert b'<!-- Bootstrap CSS -->' in response.data
