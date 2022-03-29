11ab1d7454d7a2bea2d1f04c7a757787d5e73a63
acmp17 <alliewxy@gmail.com>
2022-03-29 09:48:05 -0400

    testing docker_page_test.py: added more content to cover testing python_flask_page_test.py: added header stuff

--- tests/python_flask_page_test.py
+++ tests/python_flask_page_test.py
@@ -2,5 +2,12 @@ def test_request_python_flask_content(client):
     """This tests the python flask info page"""
     response = client.get("/page3")
     assert response.status_code == 200
+    assert b"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Python/Flask</title>
+    </head>
+    <body>""" in response.data
     assert b'<h2>Pytest</h2>' in response.data
     assert b"<h2>Outline</h2>" in response.data
